d520cc376fcd5f4a00be8320eb79306b4e0a0598
Arman <armandwi.pangestu7@gmail.com>
2023-09-19 16:32:33 +0700

    feat: add another method (get, post, delete, put, patch)

--- src/index.js
+++ src/index.js
@@ -9,11 +9,142 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/products", async (req, res) => {
   const product = await prisma.product.findMany();
-  res.send({
+
+  res.status(200).send({
     data: product,
-    message: "200 OK"
+    status_code: "200 OK",
+    message: "Get Product",
+  });
+});
+
+app.post("/products", async (req, res) => {
+  //   const newProduct = req.body;
+  const { name, price, description, image } = req.body;
+
+  const product = await prisma.product.create({
+    // data: {
+    //   name: newProduct.name,
+    //   price: newProduct.price,
+    //   description: newProduct.description,
+    //   image: newProduct.image,
+    // },
+    data: {
+      name,
+      price,
+      description,
+      image,
+    },
+  });
+
+  res.status(201).send({
+    data: product,
+    status_code: "201 Created",
+    message: "Product Added",
+  });
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await prisma.product.delete({
+    where: {
+      id,
+    },
+  });
+
+  // res.status(202).send({
+  //     status_code: "204 Accepted",
+  //     message: "Product Deleted",
+  // });
+
+  res.status(204).send({
+    status_code: "204 No Content",
+    message: "Product Deleted",
+  });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  // const productData = req.body
+  const { name, price, description, image } = req.body;
+
+  if (!(name, price, description, image)) {
+    return res.status(400).send({
+      status_code: "400 Bad Request",
+      message: "Some fields are missing",
+    });
+
+    // return;
+  }
+
+  const product = await prisma.product.update({
+    where: {
+      id,
+    },
+    // data: {
+    //   name: productData.name,
+    //   price: productData.price,
+    //   description: productData.description,
+    //   image: productData.image,
+    // },
+    data: {
+      name,
+      price,
+      description,
+      image,
+    },
+  });
+
+  // res.status(201).send({
+  //     data: product,
+  //     status_code: "201 Create",
+  //     message: "Product Edited",
+  // });
+
+  res.status(200).send({
+    data: product,
+    status_code: "200 OK",
+    message: "Product Edited",
+  });
+});
+
+app.patch("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  // const productData = req.body
+  const { name, price, description, image } = req.body;
+
+  const product = await prisma.product.update({
+    where: {
+      id,
+    },
+    // data: {
+    //   name: productData.name,
+    //   price: productData.price,
+    //   description: productData.description,
+    //   image: productData.image,
+    // },
+    data: {
+      name,
+      price,
+      description,
+      image,
+    },
+  });
+
+  // res.status(201).send({
+  //     data: product,
+  //     status_code: "201 Create",
+  //     message: "Product Edited",
+  // });
+
+  res.status(200).send({
+    data: product,
+    status_code: "200 OK",
+    message: "Product Edited",
   });
 });
 
